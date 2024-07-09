2d47f3b23b6c6e45ab0aa89ec3057ac21fc5761b
sohrab <sohrab.gh727272@gmail.com>
2024-07-09 16:42:24 +0330

    finish product detail page

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class ProductDetailPage extends Component
 {
+    public string $slug;
+
+    public function mount(string $slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    #[Title('Product Detail')]
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page',['product'=>Product::where('slug',$this->slug)->first()]);
     }
 }
--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,14 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductsPage extends Component
 {
+    use WithPagination;
     #[Title('Product')]
     public function render()
     {
-        return view('livewire.products-page');
+        return view('livewire.products-page',[
+            'products' => Product::where('is_active',true)
+                ->paginate(6),
+            'brands' => Brand::where('is_active',1)->get(['id','name','slug']),
+            'categories' => Category::where('is_active',1)->get(['id','name','slug']),
+        ]);
     }
 }
