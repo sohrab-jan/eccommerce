e5c723a70cc1945f3bc74fb24644a0c7de1d5c81
sohrab <sohrab.gh727272@gmail.com>
2024-07-09 14:12:25 +0330

    finish home page

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class HomePage extends Component
 {
+    #[Title('Home')]
+
+
     public function render()
     {
-        return view('livewire.home-page');
+        return view('livewire.home-page', [
+            'categories' => Category::where('is_active',true)->get(),
+            'brands' => Brand::where('is_active',true)->get(),
+        ]);
     }
 }
