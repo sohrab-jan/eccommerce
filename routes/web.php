ab364271338e07bd48f70a5d35a4fd2e8d566fae
sohrab <sohrab.gh727272@gmail.com>
2024-07-28 14:39:25 +0330

    add register page

--- routes/web.php
+++ routes/web.php
@@ -32,17 +32,26 @@ Route::get('/categories', CategoriesPage::class);
 
 Route::get('/products', ProductsPage::class);
 Route::get('/products/{slug}', ProductDetailPage::class);
-
 Route::get('/cart', CartPage::class);
-Route::get('/checkout', CheckoutPage::class);
 
-Route::get('/my-orders', MyOrderPage::class);
-Route::get('/my-orders/{order}', MyOrderDetailPage::class);
 
-Route::get('/login', LoginPage::class);
-Route::get('/register', RegisterPage::class);
-Route::get('/forgot', ForgotPasswordPage::class);
-Route::get('/reset', ResetPasswordPage::class);
+Route::middleware('guest')->group(function (){
+    Route::get('/login', LoginPage::class);
+    Route::get('/register', RegisterPage::class);
+    Route::get('/forgot', ForgotPasswordPage::class);
+    Route::get('/reset', ResetPasswordPage::class);
+});
 
-Route::get('/success', SuccessPage::class);
-Route::get('/cancel', CancelPage::class);
+Route::middleware('auth')->group(function (){
+    Route::get('/logout',function (){
+        auth()->logout();
+        return redirect('/');
+    });
+    Route::get('/checkout', CheckoutPage::class);
+
+    Route::get('/my-orders', MyOrderPage::class);
+    Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+    Route::get('/success', SuccessPage::class);
+    Route::get('/cancel', CancelPage::class);
+});
+
