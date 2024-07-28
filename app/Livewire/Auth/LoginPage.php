178df21ecd8e2b8efe0196b22089b3130d9f3f29
sohrab <sohrab.gh727272@gmail.com>
2024-07-28 15:17:16 +0330

    add login page

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire\Auth;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Login')]
 class LoginPage extends Component
 {
+    public $email;
+    public $password;
+
+    public function login()
+    {
+        $this->validate([
+            'email' => 'required|email|max:255|exists:users,email',
+            'password' => 'required|min:6|max:255',
+        ]);
+        if(!auth()->attempt(['email' => $this->email, 'password' => $this->password])){
+         session()->flash('error','your credentials are wrong');
+         return;
+        }
+        return redirect()->intended();
+    }
     public function render()
     {
         return view('livewire.auth.login-page');
